75f4daa9eee1fd11da7d22c97f0e431960235ef2
Israel-machine <israel.diaz1701@gmail.com>
2026-02-10 00:27:20 -0800

    lab for nested and arrow functions

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-
-
-
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+
+  const loginAttempt = (passwordAttempt) => {
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      console.log("Login successful")
+      return "Login successful";
+    } else if (attemptCount < 3) {
+      attemptCount++;
+      console.log(`Attempt ${attemptCount}: Login failed`)
+      return `Attempt ${attemptCount}: Login failed`;
+    } else {
+      attemptCount++
+      console.log("Account locked due to too many failed login attempts")
+      return "Account locked due to too many failed login attempts";
+    }
+  }
+
+  return loginAttempt
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
